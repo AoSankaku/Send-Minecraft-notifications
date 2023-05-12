829e4dcd3b9643315b58266b6e7661ad5dfb5a87
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2023-05-13 04:17:09 +0900

    Edited whole ecosystem

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Send-Minecraft-notifications
 ## これは何？
 Minecraftのサーバーのログの差分を取得し、それを元にWebHookリクエストを送信することでDiscordに通知を送ります。
-
-### こんな事ができます
-
-### これは無理です
-Discordのチャットを読み取る
+例えばサーバーの起動と閉鎖、誰かがログイン/ログアウトした情報などのほか、ログを直接参照しているため、ログに書いてあることはすべて扱うことができます。
 
 ## What is this?
-You can get a notification using WebHook everytime the server log file is changed.
+You can get a notification in your discord server using WebHook everytime the server log file is changed.
+This creates a notification on starting/shutting down your server, someone logging in or out, or every event on your log file.
 
 ## 使い方
 .batファイルを作り、
--- main.py
+++ main.py
@@ -3,49 +3,99 @@ import os, re, time, difflib, shutil, requests
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers.polling import PollingObserver
 
-webhook_url = "enter your discord webhook url here"
+# ここにDiscordのWebhookのURLを貼り付けてください。
+# Enter your discord (or other services) webhook URL here!
+webhook_url = "https://paste.your.webhook.address/here!"
 target_dir = "./logs/"
 target_file = "latest.log"
 
-
 def SendMessage(message: str) -> None:
-    # discode以外のサービスのwebhookの場合はここを変更してください
+    # discord以外のサービスのwebhookの場合はここを変更してください
+    # If you wish to use services other than discord, customize here based on your service:
     main_content = {"content": message}
     requests.post(webhook_url, main_content)
 
 def MessageCreation(text: str):
     print(text)
-    # 参加,退出以外のメッセージが必要な場合はここに書く
-    match = re.findall(": (.*) joined the game", text)
+    # 参加メッセージ
+    # When someone joined
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Server thread/INFO]: (.*) joined the game", text)
     if len(match) == 1:
         return ":green_circle: " + str(match[0]) + " が参加しました。一緒に遊んであげよう！"
-    match = re.findall(": (.*) left the game", text)
+    # 退出メッセージ
+    # When someone left
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Server thread/INFO]: (.*) left the game", text)
     if len(match) == 1:
         return ":red_circle: " + str(match[0]) + " が退出しました。"
-    match = re.findall(": (.*) For help, type \"help\"", text)
+    # サーバー起動メッセージ
+    # When your server is launched
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Server thread/INFO]: Done ", text)
     if len(match) == 1:
-        return ":door: **サーバーがオンラインになりました！**"
-    match = re.findall(": (.*) Closing Server", text)
+        return ":door: **【サーバーがオンラインになりました！】** :door:"
+    # サーバー終了メッセージ
+    # When your server is closed
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Server thread/INFO]: Closing Server", text)
     if len(match) == 1:
-        return ":no_entry_sign: **サーバーがオフラインになりました。**"
+        return ":no_entry_sign: **【サーバーがオフラインになりました。】** :no_entry_sign:"
+    # その他好きに追加・削除可能（上にあるものもいらないやつは消してOK）
+    # You can freely add or remove some messages above or below
 
+    # セキュアなチャット
+    # secure chat
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Async Chat Thread - #(.*)/INFO]: <(.*)>(.*)", text)
+    if len(match) == 1:
+        return f'`[{str(match[0][1])}]{str(match[0][2])}`'
+    # セキュアではないチャット
+    # non-secure chat
+    match = re.findall("^\[[0-9]{2}:[0-9]{2}:[0-9]{2}] \[Async Chat Thread - #(.*)/INFO]: \[(.*)] <(.*)>(.*)", text)
+    if len(match) == 1:
+        print(str(match))
+        return f'`[{str(match[0][2])}]{str(match[0][3])}`'
+    
     return None
 
 
 def GetLog(filepath: str):
-    # 最終行のテキストを取得
-    with open(filepath, "r") as f:
-        endtxt = f.readlines()[-1]
+
+     # 先に現在のログを取得（こっちを先にしないとずれる）
+    with open(filepath, "r", encoding="cp932", errors="ignore") as f:
+        log = f.readlines()
+    
+    # 前回のログを参照する（.prevファイルが存在しない場合は何も実行しない）
+    try:
+        oldLogRaw = open(f'{target_dir}{target_file}.prev', "r", encoding="cp932", errors="ignore")
+        oldLog = oldLogRaw.readlines()
+        oldLogRaw.close()
+    except FileNotFoundError:
+        oldLog = [""]
+
+    # 読み込みが終わった段階でdiff作成
+    CreateDiff()
+   
+    # latest.logの配列数がlatest.log.prevの配列数より少ない＝新規作成ファイルなのでprevの内容は無視する
+    if (len(log) < len(oldLog)):
+         oldLog = [""]
+
+    # oldLogとlogの排他的論理和を取って、一致しない部分だけを文字列としてそれぞれ送信する
+    # diff = set(oldLog) ^ set(log)
+    #
+    # 排他的論理和だと「連続で同じ文字を送信した」などの特殊な状況の場合に反応しなくなるため
+    # 「len(oldLog)-1の長さだけlogの要素を削除する」で決着
+    del log[:len(oldLog)]
+    diff = log
+    print(len(log))
+    print(len(oldLog))
 
     # 送信メッセージ作成
-    text = MessageCreation(endtxt)
+    for e in diff:
+        text = MessageCreation(e)
+        if text is not None:
+            SendMessage(text)
 
-    if text is not None:
-        SendMessage(text)
 
-def Clone():
-    #
-    shutil.copy(f'{target_dir}{target_file}', f'{target_dir}{target_file}.diff')
+def CreateDiff():
+    # 処理終了時にその時点のlatest.logをlatest.log.prevとして保存して次回比較に使用する
+    shutil.copyfile(f'{target_dir}{target_file}', f'{target_dir}{target_file}.prev')
 
 class ChangeHandler(FileSystemEventHandler):
     # フォルダ変更時のイベント
