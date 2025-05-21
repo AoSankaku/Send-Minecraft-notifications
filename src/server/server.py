398309969fe7b8abac679cd9a18d1b9c889db87e
sysnote8main <sysnote8main@gmail.com>
2025-05-21 20:39:32 +0900

    feat: remove unused files

--- src/server/server.py
+++ src/server/server.py
@@ -87,7 +87,7 @@ class Server:
             return
         self.on_event(result.event_id)
         result.text = self.replace_additional_data(result.text)
-        self.logger.info("Sending", message=result.text)
+        self.logger.debug("Sending", message=result.text)
         match result.msg_type:
             case MessageType.Player:
                 await self.webhook.sendPlayerMessage(result.text, result.playerId)
