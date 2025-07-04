a73ea443711154d2db1caf3d73f8f99702422ec1
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-07-05 02:02:37 +0900

    fix: improve error handling in main function of main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import asyncio
+import traceback
 from server.server import Server
 
 async def main():
     server = Server()
-    server.init()
-    await server.listen_file_change()
-    server.shutdown()
+    try:
+        server.init()
+        await server.listen_file_change()
+    except Exception as e:
+        error_message = traceback.format_exc()
+        print(error_message)
+        if server.webhook:
+            await server.webhook.sendServerMessage(f"An unexpected error occurred:\n```\n{error_message}```")
+    finally:
+        server.shutdown()
 
 if __name__ == "__main__":
     asyncio.run(main())
